306b3a9058ccf62a22a24807f215cbe2bfdcbc4a
papayalovers <145134417+papayalovers@users.noreply.github.com>
2024-01-06 18:58:51 +0700

    Update home.py

--- session/home.py
+++ session/home.py
@@ -9,6 +9,6 @@ with open("./stream_data/used_data.pickle",'rb') as file:
 
 # home session
 def do_home():
-    st.header('Telcome Profile Report :house:', divider='rainbow')
+    st.header('Telcom Profile Report :house:', divider='rainbow')
     profile = ProfileReport(data, title='Telcom Data Profile Report', html={'style':{'full_width':True}})
     st_profile_report(profile)
